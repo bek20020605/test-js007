78ae49d22afd367b1f83347bc2e34a829bdbce70
Bekmirzo <bekmirzoeshqulov4@gmail.com>
2025-02-01 12:46:13 +0900

    Ikkinchi push

---
+++ func.js
@@ -0,0 +1,45 @@
+//1.
+//function Aboutmyself(fullname,age,birthregion,profession) {
+   // console.log(`fullname: ${fullname};age: ${age};birthregion: ${birthregion};profession: ${profession}`);
+//}
+//Aboutmyself("Eshkulov Bekmirzo Ilkham ugli","23 years","Samarkand region","Student");
+
+//2.
+//function findtheprice(fruit){
+  //  let fruit = `olma`;
+//if (fruit == gilos || fruit == olcha) {
+  //  console.log(`50ming so'm`);
+//} else if( fruit == uzum || fruit == shaftoli){
+  //  console.log(`75000so'm`);
+//} else if ( fruit == banan || fruit == ananas){
+  //  console.log(`95 000so'm`);
+//} else {
+  //  console.log("bunaqa meva yo'q");
+//}
+//}
+//findtheprice()
+
+//3.
+function findthetime (time) {
+    if (time<=12 || time>=14){
+        console.log(`Abed`);
+    }
+    else if (time==7 || time==8 ||time==9){
+        console.log(`Ertalab`);
+    }
+    else if (time<=20 || time>=18){
+        console.log(`Kechqurun`);
+    }
+    else {
+        console.log(`Bunaqa vaqt yo'q`);
+    }
+}
+findthetime (20)
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,244 +1,102 @@
 //let a = 10;
-//let b = 3;
+//let b = 4;
 //let c = a / b;
-//console.log(c);
 //console.log(typeof c);
-//console.log(typeof c.toFixed(4));
-//console.log(Number.parseInt(c));
+//console.log(typeof c. toFixed(2));
 //console.log(Number.parseFloat(c));
-
-//console.log(Math.round(c));
-//console.log(Math.ceil(c));
-//console.log(Math.floor(c));
-
-//console.log(Math.sqrt(c));
+//console.log(Number.parseInt(c));
 //console.log(Math.cbrt(c));
-
-//console.log(c.toFixed(2));
-//let numlist = [2,4,6,8,9];
+//console.log(c.toFixed(1));
+//let numlist = [2,4,6,8];
 //console.log(Math.min(numlist));
 
+//let numlist = [2,4,6,7,8];
+//console.log(Math.min(numlist));
 //let numlist = [];
-//console.log(Math.min(2,4,6,8,10));
-//console.log(Math.max(2,4,6,8,10));
+//console.log(Math.min(2,4,6,8,9,10));
+//console.log(Math.max(2,4,6,8,9,10));
 //console.log(Math.random());
-//console.log(Number(22).toString(2));
-
-//let str = `Eshkulov`;
-//console.log(`Family:`,str);
-//console.log(`Family: ${str}`);
-//let str = `Eshkulov,Suyarov,Ilhomov`;
+//console.log(Number(32).toString (7));
+//console.log(Number(34).toString(3));
+//let str =(`Bekmirzo`);
+//console.log(`Name:`,str);
+//console.log(`Name:${str}`);
+//let str = `samarqand,toshkent,surxondaryo,qashqadaryo,buxoro`;
 //console.log(str.length);
-////console.log(str[14]);
-//console.log(str.indexOf("Suyarov"));
-//console.log(str.includes("Ilhomov"));
-//console.log(str.search("Eshkulov"));
-
-//console.log(str.slice(9,14));
-//console.log(str.substr(9,14));
-//console.log(str.substring(9,14));
-
-//console.log(str.replace("Eshkulov","Ikromov"));
-//console.log(str.replace(/suyarov/id,"Bekmirzayev"));
-
+//console.log(str[15]);
+//console.log(str.indexOf("surxondaryo"));
+//console.log(str.includes("surxon"));
+//console.log(str.search("buxoro"));
+//console.log(str.slice(10,19));
+//console.log(str.substring(10,20));
+//console.log(str.replace("toshkent","farg'ana"));
+//console.log(str.replace(/tashken/i,"sirdaryo"));
+//console.log(str.toUpperCase());
+//c//onsole.log(str.toLowerCase());
+//let str = `My name is Bekmirzo`;
+//console.log(str.length);
+//let str = `Samarqand,Buxoro,Toshkent,Andijon,Jizzax`;
+//console.log(str.search("Toshkent"));
+//console.log(str.includes("Samarqand"));
+//console.log(str.replace("Toshkent","Farg'ana"));
+//let str = (`Samarqand,Toshkent,Jizzax,Andijon`);
 //console.log(str.toUpperCase());
 //console.log(str.toLowerCase());
+//function findthecolor(){
+  //let color = `red`;
 
-
-
-//let a = 20;
-//let b = 6;
-//let c = a/b;
-//console.log(c);
-//console.log(typeof c);
-//console.log(typeof c.toFixed(2));
-//console.log(Number.parseInt(c));
-//console.log(Number.parseFloat(c));
-
-//console.log(Math.round(c));
-//console.log(Math.ceil(c));
-//console.log(Math.floor(c));
-
-//console.log(Math.sqrt(c));
-//console.log(Math.cbrt(c));
-
-//console.log(c.toFixed(3));
-
-//console.log(c.toFixed(2));
-//console.log(c.toFixed(2));
-//console.log(c.toFixed(2));
-//console.log(c.toFixed(2));
-//console.log(c.toFixed(2));
-//console.log(c.toFixed(2));
-
-//let numlist = [2,4,6,8,9];
-//console.log(Math.min(numlist));
-
-//let numlist = [2,4,6,8,9];
-//console.log(Math.min(numlist));
-//let numlist = [];
-//console.log(Math.min(2,4,6,8,10));
-//console.log(Math.min(2,4,6,8,10));
-//console.log(Math.max(2,4,6,8,10));
-
-//let numlist = [];
-//console.log(Math.min(1,2,3,4,5,6,7,8,79));
-//console.log(Math.max(1,2,3,4,5,6,7,8,9));
-
-//console.log(Math.random());
-////console.log(Math.random());
-//console.log(Math.random());
-//console.log(Math.random());
-//console.log(Number(72).toString(2));
-//console.log(Number(34).toString(4));
-//console.log(Number(52).toString(2));
-
-//let str = `Ilhomivich`;
-//console.log(`Otasining ismi:`,str);
-//let str = `Eshkulov`;
-//console.log(`Family:`,str);
-//console.log(`Family: ${str}`);
-//let str = `Eshkulov`;
-//console.log(`Family:`,str);
-//console.log(`Family: ${str}`);
-
-//let str = `Jomboy,Bulung'ur,Tayloq,Urgut`;
-//console.log(str.length);
-//let str =`Bulung'ur,Jomboy,Kattaqo'rg'on,Payariq`;
-//console.log(str.length);
-
-//let str = `Jomboy,Bulung'ur,PAyariq,Kattaqo'rg'on`;
-//console.log(str[28,30,32]);
-
-//let str = `Bulung'ur,Jomboy,Payariq,Kattaqo'rg'on`;
-//console.log(str.indexOf("Jomboy"));
-//let str =`Bulung'ur,Jomboy,Kattaqo'rg'on,Payariq`;
-//console.log(str.indexOf("Kattaqo'rg'on"));
-
-//let str = `Bulung'ur,Jomboy,Kattaqo'rg'on,Payariq`;
-//console.log(str.includes("Payariq"));
-
-//let str = `Bulung'ur,Jomboy,Kattaqo'rg'on,Payariq`;
-//console.log(str.includes("bulungur"));
-//let str = `Bulung'ur,Kattaqo'rg'on,urganch,Payariq`;
-//console.log(str.search("Payariq"));
-
-//console.log(str.slice(10,15));
-//console.log(str.substr(10,15));
-//console.log(str.substring(10,15));
-//console.log(str.replace("Jomboy","Nurobod"));
-//console.log(str.replace(/jomboy/i,"Urgut"))////;
-
-//console.log(str.toUpperCase());
-//console.log(str.toLowerCase());
-
-//let numlist = [];
-//console.log(Math.min(2,3,4,5,6,7,8));
-//console.log(Math.max(2,3,4,5,6,7,8,9,0));
-//let f = Math.max(10,20,30,40);
-//let j = Math.min(20,40,60,80);
-//console.log(Math.cbrt(f*j));
-
-//let str = `Men maktabga bordim`;
-//console.log(str.replace("maktabga","o'qishga"));
-
-//function Sniper() {
-  //  let str =`Bekmirzo`
-    //console.log(`Name:`,str);
-//}
-//Sniper()
-//Sniper()
-//Sniper()
-
-//function Stiper(name,family,age) {
-  //  console.log(`Name: ${name},family: ${family},age: ${age}`);
-//}
-//Stiper ("Bekmirzo","Eshkulov","23");
-
-//function findLargest() {
-     // let num = 2;
-   //if (num >=2 && num >=3) {
-    //  console.log(`number 1 is the largest`);
-  // } else if (2 <=3 ) {
-     // console.log(`number 2 is the largest`);
-   //} else { 
-    //  console.log(`number 3`);
-  // }
-//}
-//findLargest()
-
-//function findGrade() {
-   // let mark = 112;
- //if (mark >=90 && mark <=100)  {
-   // console.log(`A+ grade`);
- //} else if (mark >= 80 && mark < 90) {
-    //console.log(`A grade`);
- //} else if (mark >= 70 && mark < 80) {
-   // console.log(`B+ grade`);
- //} else if (mark >= 60 && mark < 70) {
-   // console.log(`C grade`);
- //} else if (mark >= 50 && mark < 60) {
-   // console.log(`D grade`);
- //} else if (mark >= 40 && mark < 50) {
-   // console.log(`E grade`);
- //} else if (mark >= 0 && mark < 40) {
-   // console.log(`Student has failed`);
- //} else {
-   // console.log(`invalid marks`);
- //}
-//}
-
-// findThelargest(){
-  // let num1 = 10
-   //let num2 = 2
-   //let num3 = 3
-   //if(num1 > num2 && num1 > num3){
-     // console.log(`num1 is the largest`)
-   //}else if(num2 > num1 && num2 > num3){
-     // console.log(`num2 is the largest`)
-   //}else if(num3 > num2 && num3 > num1){
-     // console.log(`num3 is the largest`)
-   //}
-//}
-//findThelargest()
-
-//function findTriangleType(side1,side2,side3) {
-  //if (side1 == side2 && side2 == side3) {
-    //console.log(`Equilateral triangle`)
-  //} else if(side1 == side2 || side2 == side3 || side1 == side3) {
-    //console.log(`Isosceles triangle`)
-  //} else {
-    //console.log(`Scalene triangle`)
+  //if (color==`red`){
+    //console.log(`Apple`)
+  //} 
+ /// else if (color==`orange`){
+    //console.log(`Orange`)
+  //}
+  //e/lse if (color==`yellow`){
+   // console.log(`Pee`)
+  //} 
+  //else {
+    //console.log(`It's not a fruit`)
   //}
 //}
-//findTriangleType(5,5,5)
-//findTriangleType(7,10,7)
-//findTriangleType(5,10,15)
+//findthecolor()
 
-//function Findthemonths (month){
-//  if (month<1 || month>12){
-    //console.log(`invalid month`)
-  //} else if( month==2) {
-  //  console.log(`29 days`)
-//  } else if(month==4 || month==6||month==9||month==11){
-    //console.log(`30 days`)
-  //} else {
-    //console.log(`31 days`)
-  //}
+//let age = "yosh";
+//switch(age) {
+ // case  "qari":
+   // console.log(`qariyala uyiga jo'natilsin`)
+    //break;
+  //case  "yosh":
+    //console.log(`ustida ishlasin`)
+    //break;
+  //case  "o'smir":
+    //console.log(`maktabga borsin`)
+    //break;
+  //default : 
+    //console.log(`Bunday malumot yo'q`);
 //}
-//Findthemonths (13)
-function Findthemonths(month){
-  if (month<1 || month>12){
-    console.log(`invalid months`)
-  }else if(month==2){
-    console.log(`29days`)
-  } else if (month==4 || month==6 ||month==9 ||month==11){
-    console.log(`30 days`)
-  } else {
-    console.log(`31days`)
-  }
-}
-Findthemonths(16)
+//function stepper() {
+  //console.log(`one step`);
+//}
+//for (let i=0; i<=5; i++) {
+  //stepper();
+//}
+//let weather = 25;
+//while (weather>24){
+  //console.log(`bugun havo issiq.Daraja:${weather}`);
+//}
+let regionlist = ["Samarkand","Toshkent","Buxoro"];
+regionlist.push("Qashqadaryo");
+regionlist.push("Jomboy");
+regionlist.unshift("Nurobod");
+regionlist.push("Surxondaryo");
+regionlist.pop();
+regionlist.pop();
+regionlist.shift();
+console.log(regionlist);
+
+
+
+
 
 
 
