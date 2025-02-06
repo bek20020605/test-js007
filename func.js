bbf006939f06dbd2df4c83a2702fe1b7fbe67293
Bekmirzo <bekmirzoeshqulov4@gmail.com>
2025-02-06 09:51:04 +0900

    uchinchi push

--- func.js
+++ func.js
@@ -1,41 +1,192 @@
-//1.
-//function Aboutmyself(fullname,age,birthregion,profession) {
-   // console.log(`fullname: ${fullname};age: ${age};birthregion: ${birthregion};profession: ${profession}`);
-//}
-//Aboutmyself("Eshkulov Bekmirzo Ilkham ugli","23 years","Samarkand region","Student");
 
-//2.
-//function findtheprice(fruit){
-  //  let fruit = `olma`;
-//if (fruit == gilos || fruit == olcha) {
-  //  console.log(`50ming so'm`);
-//} else if( fruit == uzum || fruit == shaftoli){
-  //  console.log(`75000so'm`);
-//} else if ( fruit == banan || fruit == ananas){
-  //  console.log(`95 000so'm`);
-//} else {
-  //  console.log("bunaqa meva yo'q");
+//function findtheprice(fruit) {
+  //if (fruit == "olma" || fruit == "olcha"){
+  //  console.log(`70 ming so'm`);
+  //} else if(fruit =="banan" || fruit == "ananas"){
+   // console.log(`80 ming so'm`);
+  //} else if(fruit == "kivi" || fruit == "avakado"){
+    //console.log(`100 ming so'm`)
+  //} else {
+    //console.log(`Bunaqa meva mavjud emas`);
+  ///}
 //}
+//findtheprice("tarvuz")
+
+//function findthegrade (mark){
+  //if (mark>120 && mark<149){
+    //console.log(`3-gp`);
+  //} else if(mark>150 && mark<189){
+  //  console.log(`4-gp`);
+//  } else if(mark>190 && mark<229){
+    //console.log(`5-gp`);
+  //} else if(mark>230 && mark<300){
+//    console.log(`6-gp`);
+  //} else {
+    //console.log(`Bunaqa bal yo'q`);
+  //}
 //}
-//findtheprice()
+//findthegrade(90)
 
-//3.
-function findthetime (time) {
-    if (time<=12 || time>=14){
-        console.log(`Abed`);
-    }
-    else if (time==7 || time==8 ||time==9){
-        console.log(`Ertalab`);
-    }
-    else if (time<=20 || time>=18){
-        console.log(`Kechqurun`);
-    }
-    else {
-        console.log(`Bunaqa vaqt yo'q`);
-    }
-}
-findthetime (20)
+//function vegetables(name,colour,type,kilogram){
+ // console.log(`name: ${name};colour: ${colour};type: ${type}; kilogram: ${kilogram}`);
+//} vegetables("sabzi","qizil","bahorgi","2 kg");
 
+//function fruits (name,colour,kilogram,type){
+ // console.log(`name: ${name};colour: ${colour};kilogram: ${kilogram};type: ${type}`)
+//} fruits("olma","qizil","5kg","qishki");
+//fruits("olcha","sariq","1kg","kuzgi");
+
+//function findthelargest(num1,num2,num3){
+  //let num1 = 10;
+  //let num2 = 5;
+  //let num3 = 3;
+  //if(num1>num2){
+    //console.log(`number 1 is the largest`);
+ // } else if (num2<num3){
+   // console.log(`number2 is the largest`);
+  //} else {
+    //console.log(`number3`);
+  //}
+//}
+ //findthelargest(num3);
+
+ //function findthesides(side){
+  //if (side1==side2 && side2 ==side3 && side3 == side1){
+    //console.log(`Equilateral triangle`);
+  //} else if(side1==side2 || side2 ==side3){
+    //console.log(`Isosceles triangle`);
+  //} else {
+    //console.log(`Scalene triangle`);
+  //}
+ //} findthesides();
+
+//function findthemonths (month){
+  //if(month<1 || month>12){
+    //console.log(`Invalid month`);
+  //} else if (month==2){
+   // console.log(`29days`);
+  //} else if (month==4||month==6||month==9||month==11){
+    //console.log(`30days`);
+  //} else{
+    //console.log(`31 days`);
+  //}
+//} findthemonths(13);
+
+//function findthesides(side){
+  //let a = side1;
+  //let b = side2;
+  //let c = side3;
+  //let d = a==b;
+  //let e = b==c;
+  //let f = d==e;
+  //if(side=="f"){
+    //console.log(`Equilateral triangle`);
+  //} else if(side=="d"||side=="e"){
+    //console.log(`Isosceles triangle`);
+  //} else {
+    //console.log(`Scalene triangle`);
+ // }
+//}/ findthesides("f");
+
+//let taste = "shirin";
+//switch(taste){
+  //case "achchiq":
+    //console.log(`Yaxdek suv ich`);
+  //case "shirin":
+    //console.log(`Ko'k choy ich`);
+  //case "nordon":
+    //console.log(`Suv ichma`);
+  //default:
+    //console.log(`Umuman hech narsa yema`);      
+//}
+//function findthemansab(yulduz){
+  //if(yulduz<1 || yulduz>8){
+    //console.log(`Invalid mansab`);
+  //} else if (yulduz>2 && yulduz<4){
+    //console.log(`leytenant va kapitan`);
+  //} else if(yulduz>5 && yulduz<7){
+    //console.log(`podpolkovnik va polkovnik`);
+  //} else {
+    //console.log(`Bunaqa mansab yo'q`);
+ // }
+//}
+//f/indthemansab(88);
+
+
+//function findthemark(mark){
+  //if(mark>4.9 && mark<6){
+    //console.log(`A'lo`);
+ // } else if(mark>3 && mark<4.9){
+   // console.log(`Yaxshi`);
+  //} else if(mark>2 && mark<3.9){
+    //console.log(`Qoniqarli`);
+  //} else if(mark>1 && mark<2.9){
+    //console.log(`Qoniqarsiz`);
+  //} else{
+    //console.log(`Yiqildi`);
+ // }
+//} findthemark(4.7);
+
+
+
+//let grade = "70-80";
+//switch (grade){
+  //case "90-100":
+   // console.log(`A+`);
+  //case "80-90":
+   // console.log(`A grade`);
+  //case "70-80":
+   // console.log(`B+grade`)
+    ///break;
+  //case "60-70":
+   // console.log(`B grade`);
+  //case "50-60":
+    //console.log(`C+grade`);   
+  //case "40-50":
+    //console.log(`Fail`);       
+//}
+
+//function findthetype (name,year,memory){
+ // console.log(`Name: ${name};Year: ${year};Memory: ${memory};`)
+//} findthetype("Samsung","2009","5gb");
+//findthetype("HP","2011","12gb");
+//findthetype("MacPro","2016","20gb");
+
+//function findthetype (type){
+  //if (type=="MacPro" ||type=="HP")
+  //  console.log(`Year:2009,Memory 5gb,Perfect`);
+  //else if(type=="Samsung" || type=="Acer")
+   // console.log(`Year:2011,Memory:7gb,Good`);
+  //else if (type=="LG" || type=="LL")
+    //console.log(`Year:2016,Memory:10gb,so-so`);
+  //else{
+    //console.log(`Bunaqa komputer mavjud emas`)
+ // }
+//} /findthetype("IUGUFYS");
+
+//function findthetype(){
+  //console.log(`MacPro,samsung,Lg,Acer,HP`);
+//} for(let i=0; i<5; i++){
+  //findthetype();
+//}
+//function findthegrade (){
+ // console.log(`A+grade,A grade,B+grade,B grade`);
+///}for(i=0;i<5;i++){
+  //findthegrade();
+//}
+
+//function findthemansab(){
+ // console.log(`leytenant,Katta leytenant,kapitan,mayor,serjant,podpolkovnik,polkovnik`);
+//}for( i=0; i<5;i++){
+  //findthemansab();
+//}
+
+//let computerlist = ["MacPro","Samsung","Hp","Acer","lg"];
+//console.log(computerlist);
+
+let studentlist = ["To'laganov","Eshonboyev","Falonchiyev","Pistonchiyev"];
+console.log(studentlist);
+console.log(studentlist.pop("Qo'chqorov"));
 
 
 
